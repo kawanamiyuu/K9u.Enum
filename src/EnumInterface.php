e916f473e390478678696df10880d6ab6819d354
kawanamiyuu <i.am.really.you@gmail.com>
2019-06-19 22:48:38 +0900

    fix cs

--- src/EnumInterface.php
+++ src/EnumInterface.php
@@ -8,21 +8,21 @@ interface EnumInterface
     /**
      * @return string the name of this enum constant
      */
-    function name(): string;
+    public function name(): string;
 
     /**
      * @return string the name of this enum constant
      */
-    function __toString(): string;
+    public function __toString(): string;
 
     /**
      * @param string $name the name of the constant to return
      * @return self the enum constant of the specified  name
      */
-    static function valueOf(string $name);
+    public static function valueOf(string $name);
 
     /**
      * @return self[] all the constants of an enum type
      */
-    static function values(): array;
+    public static function values(): array;
 }
